d239f245bae4a109a0b51d34d955c9e090368638
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-09-29 13:50:33 -0500

    add style to the created S/N element

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ function displayCart() {
 function continueShopping() {
     document.getElementById('shop').style.display = 'none';
 }
+
+function getItemInfo() {
+
+}
+
+function addToCart() {
+
+}
+
+function createCartTable(n) {
+    // Creates the Elements of a table row
+    var row = document.createElement('tr');
+    var sn = document.createElement('td');
+    var itemCol = document.createElement('td');
+    var priceCol = document.createElement('td');
+    var quantityCol = document.createElement('td');
+    var buttonCol = document.createElement('td');
+
+    // Styling the Elements
+    row.style.padding = '5px';
+
+    sn.style.textAlign = 'center';
+    sn.style.padding = '10px';
+    sn.style.fontWeight = 'bold';
+    sn.style.fontSize = '18px';
+    sn.style.fontFamily = 'sans-serif';
+
+
+
+    row.appendChild(sn);
+    row.appendChild(itemCol);
+    row.appendChild(priceCol);
+    row.appendChild(quantityCol);
+    row.appendChild(buttonCol);
+}
