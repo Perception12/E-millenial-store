2760429755e7006b0a7efa20fdbd582ed7a912ee
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-09-30 02:35:34 -0500

    add event listener to change button style when clicked

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function displayCart() {
 function continueShopping() {
     document.getElementById('shop').style.display = 'none';
 }
+
+itemButtons = document.getElementsByClassName('items');
+console.log(itemButtons);
+
+for (let i = 0; i < itemButtons.length; i++) {
+    var btn = itemButtons[i];
+    btn.addEventListener('click', function(event) {
+        var buttonClicked = event.target;
+        console.log(buttonClicked.parentElement.className);
+        if (buttonClicked.innerHTML == 'ADD TO CART') {
+            styleOne(buttonClicked);
+        } else {
+            styleTwo(buttonClicked);
+        }
+    })
+}
+
+function styleOne(button) {
+    button.innerHTML = "REMOVE FROM CART";
+    button.style.backgroundColor = "#FFE9D6";
+    button.style.color = 'black';
+}
+
+function styleTwo(button) {
+    button.innerHTML = "ADD TO CART";
+    button.style.backgroundColor = "#FF7A00";
+    button.style.color = "white";
+}
