bbe0720c49d00c4701a8a1908ad9c49d4a80181d
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-09-30 03:12:38 -0500

    create the total price function

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ for (let i = 0; i < itemButtons.length; i++) {
     var btn = itemButtons[i];
     btn.addEventListener('click', function(event) {
         var buttonClicked = event.target;
-        console.log(buttonClicked.parentElement.className);
         if (buttonClicked.innerHTML == 'ADD TO CART') {
             styleOne(buttonClicked);
+            updateTotalPrice();
         } else {
             styleTwo(buttonClicked);
+            updateTotalPrice();
         }
     })
 }
@@ -32,4 +33,19 @@ function styleTwo(button) {
     button.innerHTML = "ADD TO CART";
     button.style.backgroundColor = "#FF7A00";
     button.style.color = "white";
+}
+
+function updateTotalPrice() {
+    var itemTable = document.getElementById('table');
+    var cartRows = itemTable.getElementsByClassName('cart-row');
+    var total;
+    for (let i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('item-quantity')[0];
+        var price = parseInt(priceElement.innerText.replace("&#8358;", ''));
+        var quantity = parseInt(quantityElement.innerText);
+        total += (price * quantity);
+    }
+    document.getElementById('total').innerHTML = total;
 }
