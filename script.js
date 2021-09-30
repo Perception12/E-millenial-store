2252d76368c54a49777f9e724907825698af4a8f
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-09-30 12:33:31 -0500

    make the total price function dynamically:

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ for (let i = 0; i < itemButtons.length; i++) {
         var buttonClicked = event.target;
         if (buttonClicked.innerHTML == 'ADD TO CART') {
             styleOne(buttonClicked);
-            updateTotalPrice();
+            createCart(buttonClicked);
         } else {
             styleTwo(buttonClicked);
-            updateTotalPrice();
         }
+
+        updateTotalPrice();
     })
 }
 
@@ -26,6 +27,7 @@ function styleOne(button) {
     button.innerHTML = "REMOVE FROM CART";
     button.style.backgroundColor = "#FFE9D6";
     button.style.color = 'black';
+    button.className = 'btn-danger';
 }
 
 function styleTwo(button) {
@@ -37,14 +39,115 @@ function styleTwo(button) {
 function updateTotalPrice() {
     var itemTable = document.getElementById('table');
     var cartRows = itemTable.getElementsByClassName('cart-row');
-    var total;
+    var total = 0;
     for (let i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i];
         var priceElement = cartRow.getElementsByClassName('cart-price')[0];
         var quantityElement = cartRow.getElementsByClassName('item-quantity')[0];
-        var price = parseInt(priceElement.innerText.replace("&#8358;", ''));
-        var quantity = parseInt(quantityElement.innerText);
+        var price = parseInt(priceElement.innerText.replace('₦', ''));
+        var quantity = parseInt(quantityElement.innerText.replace('-', ''));
         total += (price * quantity);
     }
     document.getElementById('total').innerHTML = total;
+}
+
+function removeFromCart() {
+
+}
+
+function createCart(eventTarget) {
+    btnTarget = eventTarget;
+    var container = btnTarget.parentElement;
+
+    var priceContainer = container.getElementsByClassName('centered')[0];
+    var itemPrice = priceContainer.getElementsByClassName('item-price')[0].innerText.replace('₦', '');
+    var itemName = container.getElementsByClassName('item-name')[0].innerText;
+
+    var row = document.createElement('tr');
+    row.className = 'cart-row';
+    var snCol = document.createElement('td');
+    var itemCol = document.createElement('td');
+    var priceCol = document.createElement('td');
+    var quantityCol = document.createElement('td');
+    var removeBtnCol = document.createElement('td');
+    removeBtnCol.className = 'btn-danger';
+
+    var incrBtn = document.createElement('span');
+    incrBtn.innerHTML = '+';
+    incrBtn.style.padding = '5px 15px';
+    incrBtn.style.backgroundColor = "#FF7A00";
+    incrBtn.style.color = "white";
+    incrBtn.style.borderRadius = "7px";
+    incrBtn.style.cursor = "pointer";
+
+    var decrBtn = document.createElement('span');
+    decrBtn.innerHTML = '-';
+    decrBtn.style.padding = '5px 15px';
+    decrBtn.style.backgroundColor = "#FF7A00";
+    decrBtn.style.color = "white";
+    decrBtn.style.borderRadius = "7px";
+    decrBtn.style.cursor = "pointer";
+
+    var removeBtn = document.createElement('span');
+    removeBtn.innerText = "Remove";
+    removeBtn.style.padding = "5px 15px";
+    removeBtn.style.backgroundColor = "#FFCD9E";
+    removeBtn.style.color = 'black';
+    removeBtn.style.borderRadius = "10px";
+    removeBtn.style.cursor = "pointer";
+
+    var quantity = document.createElement('span');
+    quantity.style.padding = "5px";
+    var qCount = 1;
+    quantity.innerHTML = qCount;
+
+    snCol.innerHTML = count;
+    snCol.style.textAlign = 'center';
+    snCol.style.padding = '10px';
+
+    itemCol.innerHTML = itemName;
+    itemCol.style.textAlign = 'center';
+    itemCol.style.padding = '10px';
+
+    priceCol.innerHTML = '₦' + itemPrice;
+    priceCol.style.textAlign = 'center';
+    priceCol.style.padding = '10px';
+    priceCol.className = 'cart-price';
+
+    quantityCol.appendChild(decrBtn);
+    quantityCol.appendChild(quantity);
+    quantityCol.appendChild(incrBtn);
+    quantityCol.style.textAlign = 'center';
+    quantityCol.style.padding = '10px';
+    quantityCol.className = 'item-quantity';
+
+    removeBtnCol.appendChild(removeBtn);
+
+    incrBtn.onclick = incrQuantity;
+    decrBtn.onclick = decrQuantity;
+
+    row.appendChild(snCol);
+    row.appendChild(itemCol);
+    row.appendChild(priceCol);
+    row.appendChild(quantityCol);
+    row.appendChild(removeBtnCol);
+
+    document.getElementById('table').appendChild(row);
+    document.getElementById('itemNumber').innerHTML = count;
+
+    function incrQuantity() {
+        qCount++;
+        quantity.innerHTML = qCount;
+        updateTotalPrice();
+    }
+
+    function decrQuantity() {
+        qCount--;
+        if (qCount < 0) {
+            alert('Item Cannot be less than zero');
+        } else {
+            quantity.innerHTML = qCount;
+            updateTotalPrice();
+        }
+    }
 }
